4483e72652eebf99259020286548be15a1d00f8f
Jakub Lemiesiewicz <jlemiesiewicz@man.poznan.pl>
2023-05-14 18:44:51 +0200

    CA-3 add migration and create pytest functionalities

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    SUBJECT_CHOICES = [
+        ('APP', 'App Support'),
+        ('PAY', 'Payment Support'),
+        ('HR', 'HR/Jobs'),
+        ('OTH', 'Other'),
+    ]
+
+    STATUS_CHOICES = [
+        ('NEW', 'New'),
+        ('PRG', 'In progress'),
+        ('RSL', 'Resolved'),
+    ]
+
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=3, choices=SUBJECT_CHOICES)
+    message = models.TextField(max_length=500)
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='NEW')
